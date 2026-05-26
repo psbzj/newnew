b2bba743fa930be6a7e33aed22fc8e47a1d94ee1
Zachary Jordan <zacharycjordan3@gmail.com>
2026-05-25 20:39:02 -0700

    a lot of changes, moving to laptop and to be in living room

--- Calculator/src/Components/Calculator.jsx
+++ Calculator/src/Components/Calculator.jsx
@@ -1,19 +1,64 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const Calculator = () => {
+
+    const [value, setValue] = useState('0');
+    const [storedValue, setStoredValue] = useState('');
+    const [operator, setOperator] = useState('');
+
+    const handleNumberClick = (digit) => {
+        setValue((prev) => (prev === '0' ? digit : prev + digit))
+    }
+
+    const handleClear = () => {
+        setValue('0');
+    }
+
+    const handleAllClear = () => {
+        setValue('0');
+        setStoredValue('');
+        setOperator('');
+    }
+
   return (
-    <div className="container">
-        <div id="header">
+    <div>
+        <div id="calculator-container">
+            <div id="header">
             <h1>Calculator</h1>
         </div>
-        <div>
-            <p id="screen">Test, will be where numbers shows up</p>
+        <div id='screen'>
+            <p>{storedValue} {}</p>
+            <p>{value}</p>
         </div>
         <div id="calculator-buttons">
-            <button>7</button><button>8</button><button>9</button>
-            <button>4</button><button>5</button><button>6</button>
-            <button>1</button><button>2</button><button>3</button>
-            <button>0</button><button>.</button><button>=</button>
+            <button onClick={() => handleClear()}>C</button>
+            <button onClick={() => handleAllClear()}>AC</button>
+            <button>&larr;</button>
+            <button>*</button>
+            
+            <button onClick={() => handleNumberClick('7')}>7</button>
+            <button onClick={() => handleNumberClick('8')}>8</button>
+            <button onClick={() => handleNumberClick('9')}>9</button>
+            <button>/</button>
+
+            <button onClick={() => handleNumberClick('4')}>4</button>
+            <button onClick={() => handleNumberClick('5')}>5</button>
+            <button onClick={() => handleNumberClick('6')}>6</button>
+            <button>+</button>
+
+            <button onClick={() => handleNumberClick('1')}>1</button>
+            <button onClick={() => handleNumberClick('2')}>2</button>
+            <button onClick={() => handleNumberClick('3')}>3</button>
+            <button>-</button>
+            
+
+            <button onClick={() => handleNumberClick('0')}>0</button>
+            <button>00</button>
+            <button >.</button>
+            <button >=</button>
+        </div>
         </div>
     </div>
     
