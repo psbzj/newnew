1e414ca1118d94a29351d0e59765a0b6fe9094b2
psbzj <zacharycjordan3@gmail.com>
2026-05-26 09:53:05 -0700

    changes

--- Calculator/src/Components/Calculator.jsx
+++ Calculator/src/Components/Calculator.jsx
@@ -61,7 +61,8 @@ const Calculator = () => {
                     return;
             }
 
-            if (!isNaN(total)) {
+            if (typeof total === 'number' && !isNaN(total)) {
+                total = Number(total.toFixed(4))
                 setStoredValue((prev) => (prev + operator + value));
                 setValue(String(total));
                 setOperator('');
@@ -73,39 +74,39 @@ const Calculator = () => {
     <div>
         <div id="calculator-container">
             <div id="header">
-            <h1>Calculator</h1>
-        </div>
-        <div id='screen'>
-            <p id='stored-value'>{storedValue} {operator}</p>
-            <p id='curr-value'>{value}</p>
-        </div>
-        <div id="calculator-buttons">
-            <button onClick={handleClear}>C</button>
-            <button onClick={handleAllClear}>AC</button>
-            <button onClick={handleBackspace}>&larr;</button>
-            <button onClick={() => handleOperator('*')}>*</button>
-            
-            <button onClick={() => handleNumberClick('7')}>7</button>
-            <button onClick={() => handleNumberClick('8')}>8</button>
-            <button onClick={() => handleNumberClick('9')}>9</button>
-            <button onClick={() => handleOperator('/')}>/</button>
+                <h1>Calculator</h1>
+            </div>
+            <div id='screen'>
+                <p id='stored-value'>{storedValue} {operator}</p>
+                <p id='curr-value'>{value}</p>
+            </div>
+            <div id="calculator-buttons">
+                <button onClick={handleClear}>C</button>
+                <button onClick={handleAllClear}>AC</button>
+                <button onClick={handleBackspace}>&larr;</button>
+                <button onClick={() => handleOperator('*')}>*</button>
+                
+                <button onClick={() => handleNumberClick('7')}>7</button>
+                <button onClick={() => handleNumberClick('8')}>8</button>
+                <button onClick={() => handleNumberClick('9')}>9</button>
+                <button onClick={() => handleOperator('/')}>/</button>
 
-            <button onClick={() => handleNumberClick('4')}>4</button>
-            <button onClick={() => handleNumberClick('5')}>5</button>
-            <button onClick={() => handleNumberClick('6')}>6</button>
-            <button onClick={() => handleOperator('+')}>+</button>
+                <button onClick={() => handleNumberClick('4')}>4</button>
+                <button onClick={() => handleNumberClick('5')}>5</button>
+                <button onClick={() => handleNumberClick('6')}>6</button>
+                <button onClick={() => handleOperator('+')}>+</button>
 
-            <button onClick={() => handleNumberClick('1')}>1</button>
-            <button onClick={() => handleNumberClick('2')}>2</button>
-            <button onClick={() => handleNumberClick('3')}>3</button>
-            <button onClick={() => handleOperator('-')}>-</button>
-            
+                <button onClick={() => handleNumberClick('1')}>1</button>
+                <button onClick={() => handleNumberClick('2')}>2</button>
+                <button onClick={() => handleNumberClick('3')}>3</button>
+                <button onClick={() => handleOperator('-')}>-</button>
+                
 
-            <button onClick={() => handleNumberClick('0')}>0</button>
-            <button>00</button>
-            <button >.</button>
-            <button onClick={handleCalculate}>=</button>
-        </div>
+                <button onClick={() => handleNumberClick('0')}>0</button>
+                <button>00</button>
+                <button >.</button>
+                <button onClick={handleCalculate}>=</button>
+            </div>
         </div>
     </div>
     
